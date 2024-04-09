6abc7774e55414942c8b04b2fbe1afea8ca3d090
Taca0113 <shandazhan@gmail.com>
2024-04-09 13:09:53 +0200

    add file

--- SQL hazifeladat.sql
+++ SQL hazifeladat.sql
@@ -3,11 +3,3 @@ SELECT MIN(nettoar) AS Minimum,
        AVG(nettoar) AS Átlag, 
        STDEV(nettoar) AS Szórás
 FROM dvd
-
-SELECT MIN(LEN(cim)) AS Minimum_hossz, 
-       MAX(LEN(cim)) AS Maximum_hossz, 
-       AVG(LEN(cim)) AS Átlagos_hossz, 
-       STDEV(LEN(cim)) AS Szórás_hossz
-FROM dvd
-WHERE cim IS NOT NULL
-AND LEN(cim) > 0
